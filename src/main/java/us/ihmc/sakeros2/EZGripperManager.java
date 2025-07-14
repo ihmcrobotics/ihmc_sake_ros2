a76e1b6baf2ae393f54333635a13ced47a4a05da
Tomasz Bialek <tbialek@ihmc.org>
2025-07-14 13:40:59 -0500

    Added comments, fixed some manager logic

--- src/main/java/us/ihmc/sakeros2/EZGripperManager.java
+++ src/main/java/us/ihmc/sakeros2/EZGripperManager.java
@@ -1,5 +1,8 @@
 package us.ihmc.sakeros2;
 
+/**
+ * Manages higher-level control of an EZGripper, including calibration, position control, error resets, and automatic cooldowns.
+ */
 public class EZGripperManager
 {
    public enum OperationMode
@@ -19,7 +22,7 @@ public class EZGripperManager
       }
    }
 
-   private static final byte DISABLE_AUTO_COOLDOWN = (byte) 255;
+   public static final byte DISABLE_AUTO_COOLDOWN = (byte) 255;
 
    private final EZGripperInterface gripper;
 
@@ -46,18 +49,35 @@ public class EZGripperManager
       return gripper;
    }
 
+   /**
+    * Updates the gripper commands based on operation mode. Should be called periodically.
+    */
    public void update()
    {
       // Check whether the gripper needs to cooldown if the auto cooldown isn't disabled
       if (temperatureLimit != DISABLE_AUTO_COOLDOWN && checkCooldown())
          return;
 
+      // If an error needs to be reset, do so as second priority
+      if (desiredOperationMode == OperationMode.ERROR_RESET)
+      {
+         operationMode = OperationMode.ERROR_RESET;
+         updateErrorReset();
+         return;
+      }
+
+      // If calibration is requested or gripper is currently calibrating, update the calibration
+      if (desiredOperationMode == OperationMode.CALIBRATION || operationMode == OperationMode.CALIBRATION)
+      {
+         operationMode = OperationMode.CALIBRATION;
+         updateCalibration();
+         return;
+      }
+
       operationMode = desiredOperationMode;
       switch (operationMode)
       {
          case POSITION_CONTROL -> updatePositionControl();
-         case CALIBRATION -> updateCalibration();
-         case ERROR_RESET -> updateErrorReset();
          case COOLDOWN -> checkCooldown();
       }
    }
@@ -119,36 +139,77 @@ public class EZGripperManager
       gripper.setTorqueOn(false);
    }
 
+   /**
+    * Set the goal position. 0.0 = fully closed, 1.0 = fully open.
+    *
+    * @param goalPosition The desired position.
+    */
    public void setGoalPosition(float goalPosition)
    {
       this.goalPosition = goalPosition;
    }
 
+   /**
+    * <p>Set the maximum effort to be used to achieve the goal position. 0.0 = no effort, 1.0 = max effort.</p>
+    * <p>0.0 = no effort (fingers will not move), 1.0 = maximum effort (can quickly overheat actuator).
+    * 0.3 is a reasonable normal value, and it is recommended not to exceed 0.8.</p>
+    *
+    * @param maxEffort
+    */
    public void setMaxEffort(float maxEffort)
    {
       this.maxEffort = maxEffort;
    }
 
+   /**
+    * Set whether the gripper should have its torque on. Keeping the torque off when not needed can help keep the gripper's temperature down
+    *
+    * @param torqueOn Whether to have the torque on.
+    */
    public void setTorqueOn(boolean torqueOn)
    {
       this.torqueOn = torqueOn;
    }
 
+   /**
+    * Set the desired operation mode.
+    *
+    * @param operationMode The desired operation mode.
+    */
    public void setOperationMode(OperationMode operationMode)
    {
       this.desiredOperationMode = operationMode;
    }
 
+   /**
+    * Get the current operation mode. Depending on the state of the manager,
+    * the current operation mode may not be the desired operation mode.
+    *
+    * @return The current operation mode.
+    */
    public OperationMode getOperationMode()
    {
       return operationMode;
    }
 
+   /**
+    * Whether the gripper has been calibrated.
+    *
+    * @return {@code true} if the gripper has been calibrated. {@code false} otherwise.
+    */
    public boolean isCalibrated()
    {
       return isCalibrated;
    }
 
+   /**
+    * <p>Set the temperature limit.</p>
+    * <p>The manager will automatically go into cooldown mode if the gripper exceed this temperature.
+    * Cooldown will continue until the gripper's temperature drops by 10% of the temperature limit.</p>
+    * <p>To override or disable automatic cooldown, set the temperature limit to {@link #DISABLE_AUTO_COOLDOWN}</p>
+    *
+    * @param temperatureLimit The temperature limit, in Celsius.
+    */
    public void setTemperatureLimit(byte temperatureLimit)
    {
       this.temperatureLimit = temperatureLimit;
